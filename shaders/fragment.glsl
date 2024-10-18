1228dd33578fd9975e3e8bf1c4cae623a0596989
Daste <stefankar1000@gmail.com>
2024-10-18 23:24:04 +0200

    Format shader files

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,7 +1,8 @@
 #version 410 core
 
-layout (location = 0) in vec3 fragmentColor;
-out vec4 finalColor;
+layout(location = 0) in vec3 fragmentColor;
+
+layout(location = 0) out vec4 finalColor;
 
 void main() {
     finalColor = vec4(fragmentColor, 1.0);
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -2,6 +2,7 @@
 
 layout(location = 0) in vec3 vertexPosition;
 layout(location = 1) in vec3 vertexColor;
+
 layout(location = 0) out vec3 fragmentColor;
 
 void main() {
